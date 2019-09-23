bd692c59f2d02ee3f9efefd9466aac7e30c1be21
yanbowen1994 <511838976@qq.com>
2019-09-23 09:24:33 +0800

    Add router compile env set, include arm-gcc arm-ld, arm openssl dir.

--- env.sh
+++ env.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 export OPENSSL_DIR="/usr/local"
 export OPENSSL_STATIC="1"
+export ARM_UNKNOWN_LINUX_MUSLEABIHF_OPENSSL_DIR=/root/openwrt/openwrt-sdk-18.06.4-ipq806x_gcc-7.3.0_musl_eabi.Linux-x86_64/staging_dir/target-arm_cortex-a15+neon-vfpv4_musl_eabi/usr
+export PATH+=:/root/openwrt/openwrt-sdk-18.06.4-ipq806x_gcc-7.3.0_musl_eabi.Linux-x86_64/staging_dir/toolchain-arm_cortex-a15+neon-vfpv4_gcc-7.3.0_musl_eabi/bin

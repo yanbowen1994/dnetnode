bae32b77b8a137aa50ada74baee221fcbc81163f
yanbowen1994 <511838976@qq.com>
2019-11-25 15:03:51 +0800

    tinc错误信息将通过tcp连接上报, 不需要再捕获tinc system信息

--- tinc-plugin/src/operator/check.rs
+++ tinc-plugin/src/operator/check.rs
@@ -7,62 +7,20 @@ use super::{PID_FILENAME, TINC_MEMORY_LIMIT, TINC_ALLOWED_OUT_MEMORY_TIMES};
 
 impl TincOperator {
     pub fn check_tinc_status(&self) -> Result<()> {
-        #[cfg(all(not(target_arch = "arm"), not(feature = "router_debug")))]
-            {
-                let tinc_handle = self.tinc_handle
-                    .lock()
-                    .unwrap();
-                let child = tinc_handle
-                    .as_ref()
-                    .ok_or(Error::TincNeverStart)?;
-                let out = child.try_wait()
-                    .map_err(|_|Error::TincNotExist)?;
-                if let None = out {
-                    return Ok(());
-                }
-                if let Some(out) = out {
-                    if let Some(code) = out.status.code() {
-                        let error = String::from_utf8_lossy(&out.stderr).to_string();
-
-                        if error.contains("Address already in use") {
-                            error!("port 50069 already in use");
-                            return Err(Error::AnotherTincRunning);
-                        }
-
-                        if error.contains("Device or resource busy") {
-                            error!("Device or resource busy");
-                            return Err(Error::AnotherTincRunning);
-                        }
-
-                        if error.contains("Terminating") {
-                            return Err(Error::TincNotExist);
-                        }
-
-                        if error.contains("Ready") {
-                            return Ok(());
-                        }
-                        error!("code:{} error:{:?}", code, error);
-                        return Err(Error::TincNotExist);
-                    }
-                }
-            }
-        #[cfg(all(target_os = "linux", any(target_arch = "arm", feature = "router_debug")))]
-            {
-                let mut res1 = Command::new("ps")
-                    .stdout(Stdio::piped())
-                    .spawn()
-                    .unwrap();
-                let res2 = Command::new("grep")
-                    .arg("tincd")
-                    .stdin(res1.stdout.take().unwrap())
-                    .output()
-                    .unwrap();
-                let res = String::from_utf8_lossy( &res2.stdout).to_string();
-                let _ = res1.wait();
-                if !res.contains("config") {
-                    return Err(Error::TincNotExist);
-                }
-            }
+        let mut res1 = Command::new("ps")
+            .stdout(Stdio::piped())
+            .spawn()
+            .unwrap();
+        let res2 = Command::new("grep")
+            .arg("tincd")
+            .stdin(res1.stdout.take().unwrap())
+            .output()
+            .unwrap();
+        let res = String::from_utf8_lossy( &res2.stdout).to_string();
+        let _ = res1.wait();
+        if !res.contains("config") {
+            return Err(Error::TincNotExist);
+        }
 
         return Ok(());
     }

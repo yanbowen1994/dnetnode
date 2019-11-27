c53f8be7c7a58afe78f8bbc64259f7e013eec6b9
yanbowen1994 <511838976@qq.com>
2019-11-27 14:58:38 +0800

    fix build script.

--- compile_script/vagrant/build.py
+++ compile_script/vagrant/build.py
@@ -8,7 +8,7 @@ try:
 except:
     pass
 
-if len(sys.argv) > 1:
+if len(sys.argv) > 1 and sys.argv[1] == "init":
     os.system("cp /usr/lib/x86_64-linux-gnu/liblzo2.so " + tinc_lib_dir + "/liblzo2.so.2")
     os.system("cp /usr/lib/x86_64-linux-gnu/libncurses.so " + tinc_lib_dir + "/libncurses.so.5")
 
@@ -17,7 +17,7 @@ if len(sys.argv) > 1:
         try:
             os.system("git clone --branch OpenSSL_1_1_1c"
                       " https://github.com/openssl/openssl.git " + openssl_dir)
-            os.system("cd " + openssl_dir)
+            os.chdir(openssl_dir)
             os.system("chmod 777 config")
             os.system("./config shared")
             os.system("make")
@@ -27,12 +27,12 @@ if len(sys.argv) > 1:
             pass
 
     fec_dir = "/root/libmyfec"
-    os.system("cd /root")
+    os.chdir("/root")
     os.system("git clone http://bowen.yan:siteview123%21%40%23@git.dnetlab.com/dnet/libmyfec.git "
               + fec_dir)
-    os.system("cd " + fec_dir)
+    os.chdir(fec_dir)
     os.system("mkdir build")
-    os.system("cd build")
+    os.chdir("build")
     os.system("cmake ..")
     os.system("make")
     os.system("sudo make install")
@@ -41,10 +41,10 @@ if len(sys.argv) > 1:
     os.system("cp /usr/local/lib/libmyfec.so /root/tinc/lib")
 
     readline_dir = "/root/readline-8.0"
-    os.system("cd /root")
+    os.chdir("/root")
     os.system("wget http://ftp.gnu.org/gnu/readline/readline-8.0.tar.gz")
     os.system("tar -zxvf readline-8.0.tar.gz")
-    os.system("cd " + readline_dir)
+    os.chdir(readline_dir)
     os.system("chmod 777 configure")
     os.system("./configure --enable-shared")
     os.system("make")
@@ -52,7 +52,7 @@ if len(sys.argv) > 1:
     os.system("cp /usr/lib/x86_64-linux-gnu/libreadline.so " + tinc_lib_dir + "/libreadline.so.8")
 
     cargo_dir = "/root/.cargo"
-    os.system("cd /root")
+    os.chdir("/root")
     while True:
         try:
             os.system("wget -O rustup.sh https://sh.rustup.rs")
@@ -62,53 +62,55 @@ if len(sys.argv) > 1:
         except:
             pass
 
+if len(sys.argv) == 1 or (len(sys.argv) > 1 and sys.argv[1] == "tinc"):
+    tinc_dir = "/root/tinc_src"
+    if not os.path.exists(tinc_dir):
+        os.chdir("/root")
+        os.system("git clone -b 1.2 http://bowen.yan:siteview123%21%40%23@git.dnetlab.com/dnet/tinc.git "
+             + tinc_dir)
+        os.chdir(tinc_dir)
+    else:
+        os.chdir(tinc_dir)
+        os.system("git pull")
 
-tinc_dir = "/root/tinc_src"
-if not os.path.exists(tinc_dir):
-    os.system("cd /root")
-    os.system("git clone -b 1.2 http://bowen.yan:siteview123%21%40%23@git.dnetlab.com/dnet/tinc.git "
-         + tinc_dir)
-    os.system("cd " + tinc_dir)
-else:
-    os.system("cd " + tinc_dir)
-    os.system("git pull")
+    os.chdir(tinc_dir)
+    os.system("autoreconf -fsi")
+    os.system("chmod 777 configure")
+    os.system("./configure --with-openssl-lib=/root/openssl/"
+              " --with-openssl-include=/root/openssl/include"
+              " --with-readline-lib=/root/readline-8.0/shlib/"
+              " --with-readline-include=/root/readline-8.0/include")
+    os.system("make")
 
-os.system("cd " + tinc_dir)
-os.system("autoreconf -fsi")
-os.system("chmod 777 configure")
-os.system("./configure --with-openssl-lib=/root/openssl/"
-          " --with-openssl-include=/root/openssl/include"
-          " --with-readline-lib=/root/readline-8.0/shlib/"
-          " --with-readline-include=/root/readline-8.0/include")
-os.system("make")
+if len(sys.argv) == 1 or (len(sys.argv) > 1 and sys.argv[1] == "dnet"):
+    dnet_dir = "/root/dnetnode"
+    if not os.path.exists(dnet_dir):
+        os.system("git clone http://bowen.yan:siteview123%21%40%23@git.dnetlab.com/dnet/dnetnode "
+                  + dnet_dir)
+        os.chdir(dnet_dir)
+    else:
+        os.chdir(dnet_dir)
+        os.system("git pull --rebase")
 
-dnet_dir = "/root/dnetnode"
-if not os.path.exists(dnet_dir):
-    os.system("git clone http://bowen.yan:siteview123%21%40%23@git.dnetlab.com/dnet/dnetnode "
-              + dnet_dir)
-    os.system("cd " + dnet_dir)
-else:
-    os.system("cd " + dnet_dir)
-    os.system("git pull --rebase")
+    os.system("export PATH='$HOME/.cargo/bin:$PATH'")
+    os.putenv("OPENSSL_DIR", "/usr/local")
+    os.putenv("OPENSSL_STATIC", "1")
+    os.chdir(dnet_dir)
+    os.system("/root/.cargo/bin/cargo build --release")
 
-os.system("export PATH='$HOME/.cargo/bin:$PATH'")
-os.putenv("OPENSSL_DIR", "/usr/local")
-os.putenv("OPENSSL_STATIC", "1")
-os.system("cd " + dnet_dir)
-os.system("/root/.cargo/bin/cargo build --release")
-
-os.system("cd /root")
-os.system("mkdir -p /root/dnetovr/DEBIAN /root/dnetovr/lib/systemd/system "
-          "/root/dnetovr/root/dnetovr /root/dnetovr/root/tinc/lib")
-os.system("cp /root/dnetnode/cert.pem ./dnetovr/root/dnetovr")
-os.system("cp /root/dnetnode/key.pem ./dnetovr/root/dnetovr")
-os.system("cp /root/dnetnode/settings.toml.example ./dnetovr/root/dnetovr/settings.toml")
-os.system("cp /root/dnetnode/target/release/dnetovr ./dnetovr/root/dnetovr")
-os.system("cp /root/dnetnode/compile_script/control  ./dnetovr/DEBIAN")
-os.system("cp /root/dnetnode/compile_script/dnetovr.service  ./dnetovr/lib/systemd/system/dnetovr.service")
-os.system("cp /root/tinc /root/dnetovr/root -rf")
-os.system("dpkg-deb -b /root/dnetovr")
-os.system("cp /root/dnetovr.deb /mnt/dnetovr")
+os.chdir("/root")
+os.system("mkdir -p /root/dnetnode/DEBIAN /root/dnetnode/lib/systemd/system "
+          "/root/dnetnode/root/dnetnode /root/dnetnode/root/tinc/lib")
+os.system("cp /root/dnetnode/cert.pem ./dnetnode/root/dnetnode")
+os.system("cp /root/dnetnode/key.pem ./dnetnode/root/dnetnode")
+os.system("cp /root/dnetnode/settings.toml.example ./dnetnode/root/dnetnode/settings.toml")
+os.system("cp /root/dnetnode/target/release/dnet-daemon ./dnetnode/root/dnetnode")
+os.system("cp /root/dnetnode/target/release/dnet ./dnetnode/root/dnetnode")
+os.system("cp /root/dnetnode/compile_script/control  ./dnetnode/DEBIAN")
+os.system("cp /root/dnetnode/compile_script/dnet.service  ./dnetnode/lib/systemd/system/dnetnode.service")
+os.system("cp /root/tinc /root/dnetnode/root -rf")
+os.system("dpkg-deb -b /root/dnetnode")
+os.system("cp /root/dnetnode.deb /mnt/")
 
 
 
